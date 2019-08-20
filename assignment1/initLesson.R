3bf090224e9632ba3032853a633e18c741e1f0e6
monicathieu <mkt2131@columbia.edu>
2019-08-20 16:07:31 -0400

    adding tidyverse and knitr to auto-install

--- assignment1/initLesson.R
+++ assignment1/initLesson.R
@@ -3,10 +3,24 @@
       # the user's working directory and thus be accessible to them
       # throughout the lesson.
 
+# Pkgs for lesson answers
+
+if (!require(tidyverse)) {
+  install.packages(tidyverse)
+} else {
+  require(tidyverse)
+}
+
 # Pkgs for custom answer tests
 
 if (!require(rlang)) {
   install.packages(rlang)
 } else {
   require(rlang)
+}
+
+# Pkgs for THE FUTURE
+
+if (!require(knitr)) {
+  install.packages(knitr)
 }
